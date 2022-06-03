769b16b086b391ebfa46b3b604b1b8971051601e
modduo <w42023@outlook.com>
2022-06-03 11:33:07 +0800

    add sprite and post process

--- src/index.js
+++ src/index.js
@@ -1,25 +1,58 @@
 import * as THREE from 'three';
-console.log("hello world")
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import  img from './texure/sprite.png'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js';
 
+console.log("hello world")
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+			
+const cameraOrtho = new THREE.OrthographicCamera( - width / 2, width / 2, height / 2, - height / 2, 1, 10 );
+				cameraOrtho.position.z = 10;
+				const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry();
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+const map = new THREE.TextureLoader().load(img);
+const material =new THREE.SpriteMaterial({
 
-			camera.position.z = 5;
+  //color:0xff00ff,
 
+  //rotation:Math.PI/4,
+
+  map: map,
+});
+
+
+const sprite = new THREE.Sprite( material );
+
+scene.add( sprite );
+sprite.scale.set(500,500,1);
+//sprite.rotation.y+=2;
+sprite.position.x-=200;
+const composer = new EffectComposer( renderer );
+const glitchPass = new GlitchPass();
+composer.addPass( new RenderPass( scene, cameraOrtho ) );
+
+composer.addPass( glitchPass );
 			function animate() {
 				requestAnimationFrame( animate );
+				if(sprite.position.x<628)
+				{
+					sprite.position.x+=1;
+				}else
+				{
+					sprite.position.x=-200;
+				}
+				
+				console.log(sprite.position.x);
+				//sprite.rotation.y+=2;
+				//renderer.render( scene, cameraOrtho);
+				composer.render();
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
-				renderer.render( scene, camera );
 			};
 
 			animate();
